9d7fa2634c55c8f0f56a503ac45d5601e4592595
Arpit Trivedi <arpittrivedi2425@gmail.com>
2026-06-22 12:43:04 +0000

    fix background color

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import { Section } from "./components/section";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-gradient-to-b from-[#351d10] from-[10%]  to-black">
     <div className="fixed inset-0 -z-10 bg-gradient-to-b from-[#351d10] from-[10%]  to-black"></div>
       <Navbar/>
       <Hero/>
       <Section/>
     
-    </>
+    </div>
 
   );
 }
